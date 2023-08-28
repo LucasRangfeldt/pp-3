1c0d16bc0b85d457f187ca7ec5e83352e91382d4
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2023-08-28 15:54:08 +0200

    added basic strutue for 5x5 board

--- run.py
+++ run.py
@@ -1,3 +1,20 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+"""Function for creating a board
+"""
+def create_board(rows, cols):
+  board = []
+  for i in range(rows):
+    board.append(['!'] * cols)
+  return board
+
+def print_board(board):
+  for row in board:
+    	print(' '.join(row))
+    
+rows, cols = 5, 5
+board = create_board(rows, cols)
+
+print_board(board)
