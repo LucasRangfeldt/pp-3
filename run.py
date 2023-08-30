9a0bf5edf0f8090e59063d82116f57b11126cd57
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2023-08-30 18:12:31 +0200

    changed visuals ships, also made opponents ships invisible until hit

--- run.py
+++ run.py
@@ -6,21 +6,38 @@ import random
 
 # Function for creating a board
 def create_board(rows, cols):
-    return [['!' for _ in range(cols)] for _ in range(rows)]
+    return [['~' for _ in range(cols)] for _ in range(rows)]
 
 
 # Function for printing the board
-def print_board(board):
-    for row in board:
-    	print(' '.join(row))
 
+def print_board(board):
+    
+    # Prints labels
+    print("  ", end="")
+    for i in range(len(board[0])):
+        print(chr(65 + i), end="  ")
+    print()
+    for i, row in enumerate(board):
+        print(i, end=" ")
+
+        # Prints the board row and only shows the visible ships
+        for cell in row:
+            if cell == 'S' or cell == 'Hit' or cell == 'Miss':
+                print(cell, end="  ")
+            else:
+                print('~', end="  ")
+        print()
+            
+    
 # Function for randomly generating ships on the board
+
 def place_random_ships(board, num_ships, target):
     placed_ships = 0
     while placed_ships < num_ships:
         row = random.randint(0, len(board) - 1)
         col = random.randint(0, len(board[0]) - 1)
-        if board[row][col] == '!':
+        if board[row][col] == '~':
             board[row][col] = target
             placed_ships += 1
             
@@ -37,9 +54,9 @@ def guess(board, row, col, target):
 rows, cols = 5, 5
 board = create_board(rows, cols)
 
-# Places five visible and five invisible ships (V) and (I)
-place_random_ships(board, 5, 'V')
-place_random_ships(board, 5, 'I')
+# Places five visible and five invisible ships S for visible and X for invis.
+place_random_ships(board, 5, 'S')
+place_random_ships(board, 5, 'X')
 
 print('Your board:')
 print_board(board)
@@ -53,7 +70,7 @@ while player_ships_left > 0 and opp_ships_left > 0:
     row = int(input("Enter row: "))
     col = int(input("Enter column:"))
     
-    if guess(board, row, col, 'I'):
+    if guess(board, row, col, 'X'):
         print("Hit!")
         opp_ships_left -= 1
     else:
@@ -63,7 +80,7 @@ while player_ships_left > 0 and opp_ships_left > 0:
     opp_row = random.randint(0, len(board) - 1)
     opp_col = random.randint(0, len(board[0]) - 1)
     
-    if guess(board, opp_row, opp_col, 'V'):
+    if guess(board, opp_row, opp_col, 'S'):
         print(f"Opponent hit your ship on on row {opp_row} and column {opp_col}!")
         player_ships_left -= 1
     else:
