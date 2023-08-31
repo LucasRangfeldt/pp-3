ed722b37ae9c437b130e76a010532983269cac63
LucasRangfeldt <lucas_rangfeldt@hotmail.com>
2023-08-31 11:22:43 +0200

    simplified a lot of the while loop, fixed indentation issues, rewrote comments

--- run.py
+++ run.py
@@ -7,34 +7,28 @@
 # Also big thanks to Mike for helping me troubleshoot.
 import random
 
-# Function for creating a board
+# Function for game board
 def create_board(rows, cols):
     return [['~' for _ in range(cols)] for _ in range(rows)]
 
-
-# Function for printing the board
-
 def print_board(board):
+    """Prints the board"""
     
-    # Prints labels
+    # Prints coordinates
     print("  ", end="")
     for i in range(len(board[0])):
         print(chr(65 + i), end="  ")
     print()
+    # Prints rows
     for i, row in enumerate(board):
         print(i, end=" ")
-
-        # Prints the board row and only shows the visible ships
         for cell in row:
             if cell == 'S' or cell == 'H' or cell == 'M':
                 print(cell, end="  ")
             else:
                 print('~', end="  ")
         print()
-            
-    
-# Function for randomly generating ships on the board
-
+#generates ships randomly on the board
 def place_random_ships(board, num_ships, target):
     placed_ships = 0
     while placed_ships < num_ships:
@@ -43,96 +37,92 @@ def place_random_ships(board, num_ships, target):
         if board[row][col] == '~':
             board[row][col] = target
             placed_ships += 1
-            
-# Function for guessing a position
+# Guess function
 def guess(board, row, col, target):
     if board[row][col] == target:
-        board[row][col] = 'H' 
+        board[row][col] = 'H'
         return True
     else:
         board[row][col] = 'M'
         return False
-    
-# main code for board
+# Main board code
 rows, cols = 5, 5
 board = create_board(rows, cols)
-
-# Places five visible and five invisible ships S for visible and X for invis.
-# The X will transform into a H if hit, or be revealed when match is over
 place_random_ships(board, 5, 'S')
 place_random_ships(board, 5, 'X')
+
+#Intro
 print("Welcome to the Battlefield!")
 print("You and the opponent(NPC) both get 5 ships each")
 print("You can both see your own ships, marked S")
 print("Whomever destroys all five enemy ships first, wins")
-print_board(board)
+
 player_ships_left = 5
 opp_ships_left = 5
-
-# Keeping track of already made guesses
 already_guessed = []
 opp_already_guessed = []
-# player makes their guess
+
+# Game loop
 while player_ships_left > 0 and opp_ships_left > 0:
-    while player_ships_left > 0 and opp_ships_left > 0:
-        while True:
-            try:
-                col = input("Enter column (A-E): ").upper()
-                if col not in ['A', 'B', 'C', 'D', 'E']:
-                    raise ValueError("Invalid coordinate, Please enter a letter between A and E.")
-                col_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
-                col_index = col_dict[col]
-                break
-            except ValueError as e:
-                print("Are you sure you are inserting the right value?")
-
-        while True:
-            try:
-                row = int(input("Enter row (0-4): "))
-                if row not in [0, 1, 2, 3, 4]:
-                    raise ValueError("Invalid coordinate, Please enter a number between 0 and 4.")
-                break
-            except ValueError as e:
-                print("Are you sure you are inserting the right value?")
-
-    try:
-        if (row, col_index) in already_guessed:
-            raise ValueError("You already guessed those coordinates. Try again.")
-        if board[row][col_index] == 'S':
-            raise ValueError("You can't target your own ship!")
-        already_guessed.append((row, col_index))
-        if guess(board, row, col_index, 'X'):
-            print(f"You hit the opponent's ship at row {row} and column {col}!")
-            opp_ships_left -= 1
-        else:
-            print("You missed!")
-    except ValueError as e:
-        print(e)
-        continue
-
-    opp_row = random.randint(0, len(board) - 1)
-    opp_col = random.randint(0, len(board[0]) - 1)
-    if (opp_row, opp_col) not in opp_already_guessed:
-        opp_already_guessed.append((opp_row, opp_col))
-        if guess(board, opp_row, opp_col, 'S'):
-            print(f"Opponent hit your ship on on row {opp_row} and column {opp_col}!")
-            player_ships_left -= 1
-        else:
-            print("Opponent missed!")
-            
     print_board(board)
     
+    # Player's guess, makes sure input is valid
+    while True:
+        try:
+            col = input("Enter column (A-E): ").upper()
+            col_dict = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4}
+            col_index = col_dict[col]
+            break
+        except KeyError:
+            print("Invalid coordinate. Please enter a letter between A and E.")
+            continue
+
+    while True:
+        try:
+            row = int(input("Enter row (0-4): "))
+            if row in range(0, 5):
+                break
+            else:
+                print("Invalid coordinate. Please enter a number between 0 and 4.")
+        except ValueError:
+            print("Invalid input. Please enter a number between 0 and 4.")
+            
+    if (row, col_index) in already_guessed:
+        print("You already guessed those coordinates. Try again.")
+        continue
+    already_guessed.append((row, col_index))
+    
+    if guess(board, row, col_index, 'X'):
+        print(f"You hit the opponent's ship at row {row} and column {col}!")
+        opp_ships_left -= 1
+    else:
+        print("You missed!")
+    
+    # NPC guess
+    opp_row, opp_col = random.randint(0, rows-1), random.randint(0, cols-1)
+    while (opp_row, opp_col) in opp_already_guessed:
+        opp_row, opp_col = random.randint(0, rows-1), random.randint(0, cols-1)
+    opp_already_guessed.append((opp_row, opp_col))
+
+    if guess(board, opp_row, opp_col, 'S'):
+        print(f"Opponent hit your ship at row {opp_row} and column {chr(65 + opp_col)}!")
+        player_ships_left -= 1
+    else:
+        print("Opponent missed!")
+
+# Announces winner
 if player_ships_left == 0:
     print("You lost, better luck next time!")
 elif opp_ships_left == 0:
     print("You won, congratulations!")
     
+# Reveal entire board when finished
 print("Revealing all ships:")
 for i, row in enumerate(board):
     print(i, end=" ")
     for cell in row:
         if cell == 'X':
-            print('N', end=" ")
+            print('N', end="  ")
         else:
-            print(cell, end=" ")
+            print(cell, end="  ")
     print()
